6a9f075861aa1f7b2142e472cc4c929834692d88
Laura Domingues Martins <martinsrogerionilton@gmail.com>
2026-09-01 16:45:39 -0300

    Create triangulo-invertido.c

---
+++ estrutura-repetitiva/triangulo-invertido.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 5; j >= 1; j--)
+        {
+            if (j >= i)
+            {
+                printf("*");
+            }
+            
+        }
+        printf("\n");
+    }
+
+}
